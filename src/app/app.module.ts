60573bf50211acfae9139f1828b4096c2d1a8b72
askaridaria <askaridaria@yahoo.com>
2022-01-21 23:08:10 +0200

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DetailsComponent } from './pages/details/details.component';
+import { ArticleService } from './services/article.service';
+import { ArticleComponent } from './components/article/article.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { AddComponent } from './components/add/add.component';
+import { HomeComponent } from './pages/home/home.component';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HighlightMenuDirective } from './highlight-menu.directive';
+import { SymbolPipePipe } from './symbol-pipe.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailsComponent,
+    ArticleComponent,
+    FooterComponent,
+    ModalComponent,
+    AddComponent,
+    MenuComponent,
+    HighlightMenuDirective,
+    SymbolPipePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ArticleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
